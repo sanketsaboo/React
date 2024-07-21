ddfc3bb90a88b7d2963eecc428ae19dcb55bc260
Sanket Saboo <sanket.saboo@idfy.com>
2024-07-21 13:03:41 +0530

    Base Folder Structure

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState('title')
+  console.log("Rendered")
+  function counter(){
+    setCount((count) => count + 1)
+  }
+  function event_logger(event){
+    setTitle(event.target.value)
+    console.log("Event Value", event.target.value)
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{title}</h1>
+      <input className='input' onChange={event_logger}></input>
+      <div className="card">
+        <button onClick={counter}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
